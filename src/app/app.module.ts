89f63da44d780c66f4584da79b41aa061125df4c
Gavin Williams <gjosephw24@gmail.com>
2018-01-23 13:17:40 -0500

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { 
+  MatToolbarModule, 
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule, 
+  MatCardModule,
+  MatTableModule
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule, 
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]

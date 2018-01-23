10dc2ed31088b45c3b10860906e72b1d03e35c67
Gavin Williams <gjosephw24@gmail.com>
2018-01-23 14:20:05 -0500

    Added all routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { 
   MatToolbarModule, 
   MatButtonModule,
@@ -8,19 +10,51 @@ import {
   MatCardModule,
   MatTableModule
 } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { HomeComponent } from './components/home/home.component';
+import { EventsComponent } from './components/events/events.component';
+import { AddeventComponent } from './components/events/addevent/addevent.component';
+import { EditeventComponent } from './components/events/editevent/editevent.component';
+import { DeleteeventComponent } from './components/events/deleteevent/deleteevent.component';
+import { EventdetailsComponent } from './components/events/eventdetails/eventdetails.component';
 
+const routes = [
+  {path: 'register', component: RegistrationComponent},
+  {path: 'home', component: HomeComponent},
+  { 
+    path: 'events', children: [
+      { path: '', component: EventsComponent },
+      { path: 'add', component: AddeventComponent},
+      { path: 'edit', component: EditeventComponent},
+      { path: 'delete', component: DeleteeventComponent},
+      { path: 'details', component: EventdetailsComponent},
+    ]
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegistrationComponent,
+    HomeComponent,
+    EventsComponent,
+    AddeventComponent,
+    EditeventComponent,
+    DeleteeventComponent,
+    EventdetailsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     MatToolbarModule, 
     MatButtonModule,
     MatFormFieldModule,
